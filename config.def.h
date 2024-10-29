d703ac5194456135eb10187c1451e9b08492a296
crazyoptimist <hey@crazyoptimist.net>
2024-10-29 12:50:39 -0500

    feat: add mblocks

--- config.def.h
+++ config.def.h
@@ -22,11 +22,12 @@ static const char *const autostart[] = {
   "sh", "-c", "/home/snail/.local/init.sh", NULL,
   "feh", "--bg-scale", "/home/snail/.local/wallpaper.png", NULL,
   "/usr/lib/polkit-kde-authentication-agent-1", NULL,
+  "/usr/local/bin/mblocks", NULL,
 	NULL /* terminate */
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
