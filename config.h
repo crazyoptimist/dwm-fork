e2bf08e91e74ec968dbb126053f7c3991fdad546
crazyoptimist <hey@crazyoptimist.net>
2024-11-01 12:53:57 -0500

    feat: larger font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"Hack Nerd Font:size=10"};
+static const char *fonts[] = {"Hack Nerd Font:size=14"};
 static const char dmenufont[] = "monospace:size=10";
 
 /* gruvbox-dark color scheme */
